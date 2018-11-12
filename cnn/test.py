160bb38d6c51b702458e1e36377197d8ba0914ad
Daniel Contreras <bones2995@hotmail.com>
2018-11-11 18:33:35 -0600

    Reconocimiento por angulos

--- cnn/test.py
+++ cnn/test.py
@@ -57,27 +57,29 @@ optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 print(trainloader)
 #Training
-for epoch in range(2):  # loop over the dataset multiple times
+def training():
+    for epoch in range(2):  # loop over the dataset multiple times
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        inputs, labels = data
+	    running_loss = 0.0
+	    for i, data in enumerate(trainloader, 0):
+	        # get the inputs
+	        inputs, labels = data
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+	        # zero the parameter gradients
+	        optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+	        # forward + backward + optimize
+	        outputs = net(inputs)
+	        loss = criterion(outputs, labels)
+	        loss.backward()
+	        optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+	        # print statistics
+	        running_loss += loss.item()
+	        if i % 2000 == 1999:    # print every 2000 mini-batches
+	            print('[%d, %5d] loss: %.3f' %
+	                  (epoch + 1, i + 1, running_loss / 2000))
+	            running_loss = 0.0
 
-print('Finished Training')
+if _name_ == '_main_':
+    training()
